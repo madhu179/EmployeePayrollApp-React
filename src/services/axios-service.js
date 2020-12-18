b312e93a0febccd5c21f9ed90d93545d7d8833ef
madhu179 <madhubabu232343@gmail.com>
2020-12-18 11:21:27 +0530

    Adding code to add Post method in Axios to be called from employee service

--- src/services/axios-service.js
+++ src/services/axios-service.js
@@ -1,5 +1,26 @@
 const axios = require("axios");
 
 class AxiosService {
+  postService(url, payload = null, tokenRequired = false, httpOptions = null) {
+    return axios.post(url, payload, tokenRequired && httpOptions)
+  }
 
+  putService(url, payload = null, tokenRequired = false, httpOptions = null) {
+    return axios.put(url, payload, tokenRequired && httpOptions);
+  }
+
+  deleteService(url, tokenRequired = false, httpOptions = null) {
+    return axios.delete(url, tokenRequired && httpOptions);
+  }
+
+  getService(url, tokenRequired = false, httpOptions = null) {
+    return axios.get(url, tokenRequired && httpOptions);
+  }
 }
+
+module.exports = {
+  postService: new AxiosService().postService,
+  putService: new AxiosService().putService,
+  deleteService: new AxiosService().deleteService,
+  getService: new AxiosService().getService,
+};
